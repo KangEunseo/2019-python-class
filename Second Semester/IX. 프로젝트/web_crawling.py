4ed5e8d4640de63a6e36498d46f435c57bf2e7bb
KangEunSeo <jbogry7@gmail.com>
2019-11-22 14:54:37 +0900

    web crawling - 가져온 웹툰 정보를 html로 파일 저장

--- Second Semester/IX. 프로젝트/web_crawling.py
+++ Second Semester/IX. 프로젝트/web_crawling.py
@@ -5,14 +5,15 @@ if __name__ == '__main__':
     data = urlopen("https://comic.naver.com/webtoon/list.nhn?titleId=651673&weekday=sat")
     soup = BeautifulSoup(data, "lxml")
 
-    cartoon_titles = soup.find_all("td", attrs={'class':'title'})
+    cartoon_titles = soup.find_all("td", attrs={'class':'title'})   #<td class="title">
+    html = "<html><head><meta charset='utf-8'></head><body>"
     for title in cartoon_titles:
-        t = title.find('a').text
-        print(t)
+        t = title.find('a').text            #제목
+        link = "http://comic.naver.com/" + title.find('a').get('href')  #링크
+        html += "<a href=%s>%s</a><br>" % (link, t)
+    html += "</body></html>"
+    output = BeautifulSoup(html, "lxml")    #htmlstring -> BeautifulSoup 객체
+    prettyHtml = str(output.prettify())     #html 줄 예쁘게
 
-    # data = urlopen("http://webtoon.daum.net/webtoon/view/guricomeback")
-    # soup = BeautifulSoup(data, "lxml")
-    #
-    # cartoon_titles = soup.find_all("strong", attrs={'class': 'tit_wt'})
-    # for title in cartoon_titles:
-    #     print(title)
+    with open("cellsOfYumi.html", mode="w", encoding="utf-8") as f:
+        f.write(prettyHtml)
